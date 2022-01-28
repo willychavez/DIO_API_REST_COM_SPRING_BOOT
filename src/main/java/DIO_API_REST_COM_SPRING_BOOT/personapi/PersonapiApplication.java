b72f8d00def6ecae9abf0cc882fd31fd2911f156
willychavez <wchavezmareco@gmail.com>
2022-01-28 11:53:15 -0300

    first commit

---
+++ src/main/java/DIO_API_REST_COM_SPRING_BOOT/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package DIO_API_REST_COM_SPRING_BOOT.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DIO_API_REST_COM_SPRING_BOOT/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO_API_REST_COM_SPRING_BOOT.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
